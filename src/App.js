c4178888eccb823c227f7ec411ed8d7003d84039
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-12-03 15:48:42 -0200

    a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Grid, Row, Col } from "react-bootstrap";
+import MovieCarousel from "./components/movieCarousel/MovieCarousel";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    submitSearch = event => {
+        event.preventDefault();
+        alert("submitted search: " + event.target.pesquisa.value);
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Row>
+                        <Col xs={1} sm={1} md={1} lg={1} className="logo">
+                            <img src={logo} className="App-logo" alt="logo" />
+                        </Col>
+                        <Col
+                            xsOffset={4}
+                            smOffset={6}
+                            mdOffset={8}
+                            lgOffset={8}
+                            className="search"
+                        >
+                            <form onSubmit={this.submitSearch}>
+                                <input
+                                    type="text"
+                                    name="pesquisa"
+                                    placeholder="pesquisar..."
+                                />
+                                <input type="submit" name="pesquisar" />
+                            </form>
+                        </Col>
+                    </Row>
+                </header>
+                <MovieCarousel />
+            </div>
+        );
+    }
 }
 
 export default App;
